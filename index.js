b5a679dcdbaec8d3591e25d4e1a4b7f3870d7d68
jimmyroot <jimmybnewman@gmail.com>
2024-01-04 16:53:46 +0000

    Finished version

--- index.js
+++ index.js
@@ -1,54 +1,71 @@
-const accounts = [
-    {
-        id: 1,
-        title: "Main Account",
-        balance: "6700.56",
-        spendings: [
-            {
-                category: "Rent",
-                spent: "1450"
-            },
-            {
-                category: "Groceries",
-                spent: "564"
-            },
-            {
-                category: "Restaurants",
-                spent: "123"
-            },
-            {
-                category: "Transport",
-                spent: "81"
-            },
-            {
-                category: "Internet",
-                spent: "50"
-            }
-        ]
-    },
-    {
-        id: 2,
-        title: "Expenses",
-        balance: "5134.63",
-        spendings: [
-            {
-                category: "Netflix",
-                spent: "19.99"
-            },
-            {
-                category: "HBO Max",
-                spent: "14.99"
-            },
-            {
-                category: "Setapp",
-                spent: "9.99"
-            }
-        ]
-    },
-    {
-        id: 3,
-        title: "Savings",
-        balance: "36500.12",
-        spendings: []
+import { accounts } from './accounts.js'
+
+const divAccounts = document.getElementById('div-container-accounts')
+const divSpending = document.getElementById('div-container-spending')
+const navBar = document.getElementById('ul-nav')
+
+divAccounts.addEventListener('click', (e) => {
+    if (e.target.dataset.id) render(e.target.dataset.id)
+})
+
+navBar.addEventListener('click', (e) => {
+    if (e.target.classList.contains('a-nav')) navigate(e.target)
+})
+
+const render = (selectedAccountID = 1) => {
+    divAccounts.innerHTML = getAccountsHtml(accounts, +selectedAccountID)
+    divSpending.innerHTML = getSpendingHtml(
+        accounts.filter(acc => acc.id === +selectedAccountID)[0].spendings
+    )
+}
+
+const getAccountsHtml = (accs, selectedAccountID) => {
+    return accs.map(account => {
+        const selected = account.id === selectedAccountID ? 'div-account-selected' : ''
+        return `
+            <div class="div-account ${selected}" data-id="${account.id}">
+                <p class="p-account-name">${account.title}</p>
+                <p>$ ${(+account.balance).toLocaleString()}</p>
+            </div>
+        `
+    }).join('')
+}
+
+const getSpendingHtml = spendings => {
+    if (spendings.length > 0) {
+        const widthsAsPercentages = getWidths(spendings)
+        return spendings.map((expense, index) => {
+            return `
+                <div class="spending-bar" style="width:${widthsAsPercentages[index]}">
+                    <p class="p-spending-name">${expense.category}</p>
+                    <p>$ ${(+expense.spent).toLocaleString()}</p>
+                </div>
+            `
+        }).join('')
+    } else {
+        return `<p class="no-expenses">You haven't spent anything from this account 🤑</p>`
     }
-]
+}
+
+const getWidths = spendings => {
+    const spends = spendings.map(spend => spend.spent)
+    const highestSpend = Math.ceil(...spends)
+    return spends.map(spend => {
+        return `${getWidthAsPercentage(spend, highestSpend, 50)}%`
+    })
+}
+
+const getWidthAsPercentage = (spend, maxSpend, minWidth) => {
+    return Math.round((minWidth) + ((minWidth + (100 - (minWidth * 2))) * (spend / maxSpend)))
+}
+
+const navigate = (target) => {
+    const existingSelection = document.querySelectorAll('.a-nav-selected')
+    if (existingSelection.length > 0) 
+        existingSelection.forEach(
+            el => el.classList.remove('a-nav-selected')
+        )
+    target.classList.add('a-nav-selected')
+}
+
+render()
